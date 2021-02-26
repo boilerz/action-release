ab3134ecd9ddb804ef9d65f187553d8283538d8e
momsse <1344228+momsse@users.noreply.github.com>
2021-02-26 15:09:21 +0100

    :fire: declaration files

--- dist/main.d.ts
+++
@@ -1,9 +0,0 @@
-interface RunOptions {
-    githubRef?: string;
-    githubToken?: string;
-    githubEmail: string;
-    githubUser: string;
-}
-export declare const defaultRunOptions: RunOptions;
-export default function run(options?: RunOptions): Promise<void>;
-export {};
